60ba938e347ff44854d2e5bd89549f6f70968776
GOUVERNEUR Sebastien <g1002677@tempo21-2.luminy.univ-amu.fr>
2016-10-17 13:19:41 +0200

    gitignore modification

--- src/app/IDate.java
+++ src/app/IDate.java
@@ -4,7 +4,7 @@ public interface IDate {
 	
 	public Date today();
 	public Date fromTimeStamp();
-	public Date fromOrdinal(Date date);
+	public Date fromOrdinal(int ordinal);
 	public Date replace(int year, int month, int day); /* d.replace(0, 0, 21) return date(currentYear, currentDay, 21)*/
 	public int toOrdinal(Date date);
 	public int weekday(Date date);
