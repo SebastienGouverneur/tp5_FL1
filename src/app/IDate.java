8c8364a71544fe4370a014076e235f64a9ad45b5
GOUVERNEUR Sebastien <g1002677@tempo21-2.luminy.univ-amu.fr>
2016-10-17 11:04:15 +0200

    init project

---
+++ src/app/IDate.java
@@ -0,0 +1,17 @@
+package app;
+
+public interface IDate {
+	
+	public Date today();
+	public Date fromTimeStamp();
+	public Date fromOrdinal(Date date);
+	public Date replace(int year, int month, int day); /* d.replace(0, 0, 21) return date(currentYear, currentDay, 21)*/
+	public int toOrdinal(Date date);
+	public int weekday(Date date);
+	public int isoWeekday(Date date);
+	public int [] isoCalendar(Date date);
+	public String isoFormat();
+	public String toString();
+	public String cTime(Date date);
+	
+}
